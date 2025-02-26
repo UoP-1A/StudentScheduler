87122537d2399a6edeb9ca737780e416927d90c1
up2208860 <up2208860@myport.ac.uk>
2025-02-26 17:22:33 +0000

    created events and can edit and delite them, also made some tests for the events thanks

--- calendarapp/models.py
+++ calendarapp/models.py
@@ -17,7 +17,7 @@ class Notification(models.Model):
     is_read = models.BooleanField(default=False)
     message = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.user.username}: {self.message}"
