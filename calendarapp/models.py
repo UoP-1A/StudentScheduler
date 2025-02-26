c20ae8245f7fe31e5ce1465ea53e062752e955da
ndunami <up2219449@myport.ac.uk>
2025-02-26 13:58:32 +0000

    Update models.py included notifications
    Attempting to add notifications for the calendar features. 
    (note: not fully developed)

--- calendarapp/models.py
+++ calendarapp/models.py
@@ -11,3 +11,13 @@ class Calendar(models.Model):
 
     def __str__(self):
         return self.user.username + " - " + self.name
+
+# Notifications. is notification read? etc below
+class Notification(models.Model):
+    is_read = models.BooleanField(default=False)
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username}: {self.message}"
