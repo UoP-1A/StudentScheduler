a58011eb214de1458a0c13aff45df7d45f5cace5
ndunami <up2219449@myport.ac.uk>
2025-02-27 16:39:25 +0000

    Update models.py

--- calendarapp/models.py
+++ calendarapp/models.py
@@ -11,13 +11,3 @@ class Calendar(models.Model):
 
     def __str__(self):
         return self.user.username + " - " + self.name
-
-# Notifications. is notification read? etc below
-class Notification(models.Model):
-    is_read = models.BooleanField(default=False)
-    message = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"{self.user.username}: {self.message}"
