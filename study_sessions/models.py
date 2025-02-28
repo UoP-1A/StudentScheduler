878bb793a3c87d12b09f52029832dfdf06937b24
gabtendelski <99609208+gabtendelski@users.noreply.github.com>
2025-02-28 15:48:48 +0100

    File structure for study sessions

---
+++ study_sessions/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from users.models import CustomUser
+
+class StudySession(models.Model):
+    host = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="study_sessions")
+    participants = models.ManyToManyField(CustomUser, related_name="study_sessions_joined", blank=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    date = models.DateField()
+    is_recurring = models.BooleanField(default=False)
+    #module
+
+    def __str__(self):
+        return self.user.username + " - " + self.title
