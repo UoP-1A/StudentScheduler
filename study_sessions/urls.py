89c2b70c0fd6f23080142c7f445f5d602733fa43
Gabriel Tendelski <gabtendelski@gmail.com>
2025-04-05 17:47:53 +0100

    redirected users to calendar after creating study session

--- study_sessions/urls.py
+++ study_sessions/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
+from django.urls import path, include
 from study_sessions import views
-from .views import get_sessions, create, create_recurring
+from .views import get_sessions, create, create_recurring, get_recurring_sessions
 
 app_name = 'study_sessions'
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path('create/', create, name='create'),
     path('create_recurring/<int:session_id>/', create_recurring, name='create_recurring'),
     path('sessions/', get_sessions, name='get_sessions'),
+    path('recurring_sessions/', get_recurring_sessions, name='get_recurring_sessions'),
 ]
 
--- study_sessions/views.py
+++ study_sessions/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.views import View
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from .models import StudySession
+from .models import StudySession, RecurringStudySession
 
 from .forms import StudySessionForm, RecurringSessionForm
 
@@ -18,7 +18,8 @@ def create(request):
             study_session.save()
 
             if study_session.is_recurring:
-                return redirect(request, 'create_recurring', session_id=study_session.id)
+                session_id = study_session.id
+                return redirect('study_sessions:create_recurring', session_id)
             else:
                 return redirect('./')
     context = {
@@ -33,12 +34,13 @@ def create_recurring(request, session_id):
         form = RecurringSessionForm(request.POST)
         if form.is_valid():
             recurring_session = form.save(commit=False)
-            recurring_session.session_id = session.id
+            recurring_session.session_id = session
             recurring_session.save()
+            return redirect('index')
     context = {
         'form': form
     }
-    return render(request, './', context)
+    return render(request, 'study_sessions/create_recurring.html', context)
 
 def get_sessions(request):
     sessions = StudySession.objects.all()
@@ -54,5 +56,16 @@ def get_sessions(request):
             'is_recurring': session.is_recurring,
             'host_id': session.host_id,
             'calendar_id': session.calendar_id_id,
+        })
+    return JsonResponse(sessions_list, safe=False)
+
+def get_recurring_sessions(request):
+    sessions = RecurringStudySession.objects.all()
+    sessions_list = []
+    for session in sessions:
+        sessions_list.append({
+            'id': session.id,
+            'recurrence_amount': session.recurrence_amount,
+            'session_id': session.session_id.id,
         })
     return JsonResponse(sessions_list, safe=False)
